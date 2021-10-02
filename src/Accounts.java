1679cc53726bc81a34a5d8d612ab58a31999b4f4
Matthew <matthewdukepan@gmail.com>
2021-10-02 15:14:07 -0400

    real classes

--- src/Accounts.java
+++
@@ -1,9 +0,0 @@
-
-public class Accounts {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- src/MainBankingDriver.java
+++
@@ -1,9 +0,0 @@
-
-public class MainBankingDriver {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
